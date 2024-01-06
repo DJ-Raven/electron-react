d0a5619b7f6a8ceab5d02f657c8205d681f6566f
raven <laingraven.java@gmail.com>
2024-01-06 21:24:23 +0700

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Hello Test Electron with React :)</div>;
+  return (
+    <div className="grid justify-items-center space-y-2">
+      <p>Hello Test Electron with React :)</p>
+      <button className="transition-colors bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white rounded-lg px-3 py-1">
+        Click here
+      </button>
+      <p className="justify-center">It's center</p>
+    </div>
+  );
 }
 
 export default App;
