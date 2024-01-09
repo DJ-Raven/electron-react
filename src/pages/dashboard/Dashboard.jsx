09ee57f704e2aa753af5bfd91653d47643d5d37b
raven <laingraven.java@gmail.com>
2024-01-09 20:36:08 +0700

    add drawer and custom scrollbar

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-5 scrollbar">
+      <p>Dashboard</p>
+      <textarea className="textarea textarea-accent" />
+    </div>
+  );
 }
 
 export default Dashboard;
